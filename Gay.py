0c894744df8cb3100c33bf59c1b674ac76e0a3a8
mdn1862 <lol4goofy23@gmail.com>
2023-10-25 17:13:07 +0200

    test

---
+++ Gay.py
@@ -0,0 +1,12 @@
+name = "4m1r" 
+zahl = 213
+zahl2 = 623
+boo = True 
+boo2 = False 
+flow = 3.1415 
+
+print(f"hallo {name}")
+
+print(zahl + zahl2)
+
+print("hallo " + name + " schoen dich kennen zu lernen ")
